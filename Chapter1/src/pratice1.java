26c016ee4d65c307cad0f9fbd90786923399efdb
KimHyeonMin2250 <61771420+KimHyeonMin2250@users.noreply.github.com>
2021-01-18 20:22:06 +0900

    Chapter1

---
+++ Chapter1/src/pratice1.java
@@ -0,0 +1,14 @@
+
+public class pratice1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a = "Welcome!!";
+		String b = "자바 세계로 오신 것을 환영합니다.";
+		
+		System.out.println(a);
+		System.out.println(b);
+		
+	}
+
+}
---
+++ Chapter1/src/pratice2.java
@@ -0,0 +1,13 @@
+
+public class pratice2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a = "Sorry~~";
+		String b = "재밌어서 미안합니다~~";
+		
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
